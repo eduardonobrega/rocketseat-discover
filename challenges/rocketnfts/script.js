edf5f9825e9af029319d72c1d6ec91ba6c9a8523
eduardo <eduardo.nobrega1999@hotmail.com>
2022-12-19 16:21:49 -0300

    gallery images

---
+++ challenges/rocketnfts/script.js
@@ -0,0 +1,21 @@
+const radios = document.querySelectorAll('.slides input');
+let count = 1;
+let width = window.innerWidth;
+
+let interval = setInterval(() => {
+  if (width < 1110) {
+    nextImage();
+  }
+}, 4000);
+
+window.addEventListener('resize', () => {
+  width = window.innerWidth;
+});
+
+function nextImage() {
+  radios[count].checked = true;
+  count++;
+  if (count == radios.length) {
+    count = 0;
+  }
+}
