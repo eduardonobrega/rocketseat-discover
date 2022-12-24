eb0d36ca47bbf54163c17a5c4b3eb63d7bf79d0f
eduardo <eduardo.nobrega1999@hotmail.com>
2022-12-23 21:36:08 -0300

    section artists

--- challenges/rocketnfts/script.js
+++ challenges/rocketnfts/script.js
@@ -5,22 +5,12 @@ let width = window.innerWidth;
 let interval = setInterval(() => {
   if (width < 1110) {
     nextImage();
-  }else {
-    document.querySelector('div.first').style.margin = 0;
-    
   }
-}, 5000);
+}, 4000);
 
 window.addEventListener('resize', () => {
   width = window.innerWidth;
 });
-
-radios.forEach(radio => {
-  const id = radio.id.slice(-1)
-  radio.addEventListener('click', () => {
-    count = id
-  })
-})
 
 function nextImage() {
   radios[count].checked = true;
