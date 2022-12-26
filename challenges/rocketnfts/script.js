e753d3690568b9bfea94d4e62c5432a06503cb6f
eduardo <eduardo.nobrega1999@hotmail.com>
2022-12-26 12:31:01 -0300

    footer

--- challenges/rocketnfts/script.js
+++ challenges/rocketnfts/script.js
@@ -1,6 +1,10 @@
 const radios = document.querySelectorAll('.slides input');
 let count = 1;
 let width = window.innerWidth;
+const password = 123;
+
+
+// essa função 
 
 let interval = setInterval(() => {
   if (width < 1110) {
